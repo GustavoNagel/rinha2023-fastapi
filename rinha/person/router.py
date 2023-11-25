5c8c29758de9eff235ceb1b16a54a44625b4f6d0
Gustavo Nagel <gust.nagel@gmail.com>
2023-11-25 17:28:13 -0300

    FIX: bugs

--- rinha/person/router.py
+++ rinha/person/router.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from rinha.person import models, schemas
 from rinha.db.database import get_db
 
@@ -11,15 +12,20 @@ def hello() -> dict[str, str]:
     return {"hello": "world!"}
 
 
-@person_router.post("/pessoas", response_model=schemas.PersonSchema)
+@person_router.post("/pessoas", response_model=schemas.PersonSchema, status_code=201)
 def create_person(
     request: schemas.PersonCreateSchema,
+    response: Response,
     db: Session = Depends(get_db),
 ) -> models.Person:
     db_person = models.Person(**request.model_dump())
     db.add(db_person)
-    db.commit()
+    try:
+        db.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=422, detail="Apelido precisa ser unico")
     db.refresh(db_person)
+    response.headers["Location"] = f"/pessoas/{db_person.id}"
     return db_person
 
 
@@ -28,7 +34,10 @@ def get_person(
     pessoa_id: str,
     db: Session = Depends(get_db),
 ) -> models.Person | None:
-    return db.query(models.Person).get(models.Person.id == pessoa_id)
+    person = db.query(models.Person).get(pessoa_id)
+    if not person:
+        raise HTTPException(status_code=404, detail='Pessoa nao encontrada!')
+    return person
 
 
 @person_router.get("/pessoas", response_model=list[schemas.PersonSchema])
@@ -36,8 +45,11 @@ def list_persons(
     t: str | None = None,
     db: Session = Depends(get_db),
 ) -> list[models.Person]:
-    print(t)
-    return db.query(models.Person).limit(50).all()  # type: ignore
+    if not t:
+        raise HTTPException(status_code=400, detail='Parametros de busca sao obrigatorios!')
+    return db.query(models.Person).filter(
+        models.Person.search_field.like(f"%{t}%")
+    ).limit(50).all()  # type: ignore
 
 
 @person_router.get("/contagem-pessoas")
@@ -45,5 +57,8 @@ def count_persons(
     t: str | None = None,
     db: Session = Depends(get_db),
 ) -> int:
-    print(t)
-    return db.query(models.Person).count()  # type: ignore
+    if not t:
+        raise HTTPException(status_code=400, detail='Parametros de busca sao obrigatorios!')
+    return db.query(models.Person).filter(
+        models.Person.search_field.like(f"%{t}%")
+    ).count()  # type: ignore
